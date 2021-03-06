2268ef17e01d71d010f59630c497adc132a0b155
zhz <zhanghengzeng1997@163.com>
2021-03-06 14:20:06 +0800

    初始化

---
+++ app/src/main/java/cn/zhz/schoolCheckIn/activity/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package cn.zhz.schoolCheckIn.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import cn.zhz.schoolCheckIn.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
